9156e630e64dcfa0d1ac4dcfac5cd61b486e32cb
IceWeedo <iceweedo@gmail.com>
2025-01-08 13:34:30 +0100

    Update run_without_txadmin.bat

--- FXServer/run_without_txadmin.bat
+++ FXServer/run_without_txadmin.bat
@@ -1,4 +1,4 @@
 @echo off
 title FXServer - GTA5
 RMDIR /s /q "./cache/files"
-start ./server-data/FXServer.exe +exec server.cfg +set gamename gta5
+start ./server-data/FXServer.exe +exec cfg/server.cfg +set gamename gta5
